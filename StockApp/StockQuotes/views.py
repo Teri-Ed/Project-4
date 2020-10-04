645b54eaa334736ce783e709cf6c364d2f7dd088
Teri Edwards <teri.edwards@outlook.com>
2020-10-04 05:41:18 -0700

    Connected to IEX Cloud API.

--- StockApp/StockQuotes/views.py
+++ StockApp/StockQuotes/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests 
+    import json
+
+    #pk_c9d8aa3cdbc143dc9af6fe0d62e7d6d4
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_c9d8aa3cdbc143dc9af6fe0d62e7d6d4")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
